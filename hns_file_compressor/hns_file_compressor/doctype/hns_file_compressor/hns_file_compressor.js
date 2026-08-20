acd8410f0ad431f5e650ca9c1c64ee4171dd10f6
Hiren Savaliya <hirenerpnext79@gmail.com>
2026-08-20 14:09:05 +0530

    feat: implement HNS File Compressor doctype with image compression logic for local and Google Drive files

---
+++ hns_file_compressor/hns_file_compressor/doctype/hns_file_compressor/hns_file_compressor.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Hns File Compressor and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("HNS File Compressor", {
+// 	refresh(frm) {
+
+// 	},
+// });
